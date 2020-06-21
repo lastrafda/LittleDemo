869c2417ca945e0e1afe6ddb57446a6688633898
José Lastra <lastrafda@gmail.com>
2020-06-21 01:11:33 -0400

    Created Person Entity

---
+++ src/main/java/com/example/LittleDemo/LittleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.LittleDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleDemoApplication.class, args);
+	}
+
+}
